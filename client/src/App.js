cb2b2046736c05c786f7cdc099ff41d610412f6e
MrAntoine <antoinevdb15@gmail.com>
2019-02-25 13:56:14 +0100

    Add routes : Home & About

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,26 @@ import {quizzes, users} from './examples';
 import {HTTP_SERVER_PORT_PICTURES} from './constants.js';
 
 
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+
+import Home from "./Home.js";
+import About from "./About.js";
+
 
 class App extends Component {
-
     render() {
-	return (
-    	  <div>
-	    Hey guys!!
-	  </div>
-	);
+        return (
+            <BrowserRouter>
+                <div>
+                    <Switch>
+                        <Route exact={true} path="/" component={Home} />
+                        <Route exact={true} path="/about" component={About} />
+                        <Route path="*" component={() => <p>Page Not Found</p>} />
+                    </Switch>
+                    Hey guys!!
+                </div>
+            </BrowserRouter>
+        );
     }
 }
 
---
+++ client/src/Home.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+import {quizzes, users} from './examples';
+import {HTTP_SERVER_PORT_PICTURES} from './constants.js';
+
+
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                Home
+            </div>
+        );
+    }
+}
+
+export default Home;
