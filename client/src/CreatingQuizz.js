c2abf860c9f8cc51bd10f649ecf3c2bed4dbb235
adrien.dufour <ad.dufour16@gmail.com>
2019-02-27 21:03:31 +0100

    CreatingQuizz add

--- client/src/CreatingQuizz.js
+++ client/src/CreatingQuizz.js
@@ -5,18 +5,40 @@ import {HTTP_SERVER_PORT_PICTURES} from './constants.js';
 
 
 class CreatingQuizz extends Component {
+    constructor(props){
+       super(props)
+       this.state={
+           quizz:null
+       }
+    }
+    createquizz(e){
+        e.preventDefault()
+        this.setState({
+            quizz:{
+                name:document.getElementById('name').value,
+                keywords:document.getElementById('categorie').value,
+                questions:{
+                    question:document.getElementById('question').value
+                }
+            }
+        })
+        console.log(this.state.quizz)
+    }
     render() {
         return(
             <div>
 
-                    <form>
-                            <input type="text" placeholder="Quizz name"/>
-                            <input type="text" placeholder="Categories"/>
-                            <input type="text" placeholder="Number of Question"/>
-                        <div id="submit">
-                            <input id="button" type="submit"/>
-                        </div>
+                    <form onSubmit={(e)=>this.createquizz(e)}>
+                            <input type="text" placeholder="Quizz name" id="name"/>
+                            <input type="text" placeholder="Categories" id="categorie"/>
 
+                            <input type="text" placeholder="Question" id="question"/>
+                            <input type="text" placeholder="Answers" id="answers1"/>
+                            <input type="text" placeholder="Answers" id="answers2"/>
+                            <input type="text" placeholder="Answers" id="answers3"/>
+                            <input type="text" placeholder="Answers" id="answers4"/>
+<br/>
+                            <input id="button" type="submit"/>
                 </form>
 
             </div>
