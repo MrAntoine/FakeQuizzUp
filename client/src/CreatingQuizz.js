5f609f50ab4b171676788a2cc014cc68bc29e45c
adrien.dufour <ad.dufour16@gmail.com>
2019-02-27 16:46:41 +0100

    CreatingQuizz add

---
+++ client/src/CreatingQuizz.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import {quizzes, users} from './examples';
+import {HTTP_SERVER_PORT_PICTURES} from './constants.js';
+
+
+class CreatingQuizz extends Component {
+    render() {
+
+    }
+}
+
+export default CreatingQuizz;
